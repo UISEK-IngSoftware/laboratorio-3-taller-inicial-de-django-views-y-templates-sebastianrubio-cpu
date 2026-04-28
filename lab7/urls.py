8f8b14c34566667ee7160cc0845b4ebacafbeb96
Sebastian <sebastian.rubio@uisek.edu.ec>
2026-04-28 10:15:05 -0500

    Creation of venv

--- lab7/urls.py
+++ lab7/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pokedex/', include('pokedex.urls')),
+    path('', include('pokedex.urls')),
 
 ]
