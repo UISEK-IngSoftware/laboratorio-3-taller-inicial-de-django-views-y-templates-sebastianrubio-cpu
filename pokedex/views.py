ee4053cd6155a3f951aa058e080329e73504a3b9
Sebastian <sebastian.rubio@uisek.edu.ec>
2026-04-28 10:41:16 -0500

    New Index.html

--- pokedex/views.py
+++ pokedex/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     pokemon = [ "pikachu", "charmander", "bulbasaur", "squirtle"] 
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'pokemon': pokemon})
