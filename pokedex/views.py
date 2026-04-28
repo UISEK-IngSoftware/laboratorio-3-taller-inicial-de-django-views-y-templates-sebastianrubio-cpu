61881ebb265560145bb68d5afd5c4142c2234cdb
Sebastian <sebastian.rubio@uisek.edu.ec>
2026-04-28 10:09:07 -0500

    Creation of venv

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    pokemon = [ "pikachu", "charmander", "bulbasaur", "squirtle"] 
+    return render(request, 'index.html')
